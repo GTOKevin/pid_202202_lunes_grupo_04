695f8ede81681dd70bed519b7589a516880eed62
Manuel David Malca Conde <i201921437@cibertec.edu.pe>
2022-04-09 14:04:45 -0500

    Agregando entidades Movimiento , Usuario y Incidente_File

---
+++ Entidades/USUARIO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+   public class USUARIO
+    {
+        public int id_usuario { get; set; }
+        public string username { get; set; }
+        public string clave { get; set; }
+        public DateTime fecha_registro { get; set; }
+        public int id_rol { get; set; }
+        public int id_perfil { get; set; }
+        public bool estado { get; set; }
+
+    }
+}
