cfbab8287383f486a0baa6a4b634afb419be1b3f
JimmyBarzola <jbarzolag7@gmail.com>
2022-04-09 14:09:45 -0500

    Creación de Entidades SECTOR y SERVICIO

--- Entidades/SECTOR.cs
+++ Entidades/SECTOR.cs
@@ -8,5 +8,9 @@ namespace Entidades
 {
     public class SECTOR
     {
+        public int id_sector { get; set; }
+        public string nombre_sector { get; set; }
+        public DateTime fecha_creacion { get; set; }
+        public int id_sucursal { get; set; }
     }
 }
---
+++ Entidades/SERVICIO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class SERVICIO
+    {
+        public int id_servicio { get; set; }
+        public int id_tipo { get; set; }
+        public int id_departamento { get; set; }
+        public string nombre { get; set; }
+        public DateTime fecha_registro { get; set; }
+    }
+}
