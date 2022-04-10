df512b88db5d0d52958f0c7eafaa04318964fad2
Manuel David Malca Conde <i201921437@cibertec.edu.pe>
2022-04-09 22:47:35 -0500

    Enviando Listado de Usuario , Incidente_file y Movimiento

---
+++ Datos/Movimiento_DA.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Entidades;
+using Helpers;
+
+namespace Datos
+{
+    public class Movimiento_DA
+    {
+        public Movimiento_Res Listar()
+        {
+            Movimiento_Res movimiento_res = new Movimiento_Res();
+            List<Movimiento> movimiento_list = new List<Movimiento>();
+            DTOHeader oHeader = new DTOHeader();
+            try
+            {
+                using (SqlConnection cn = Conexion.Conectar())
+                {
+                    cn.Open();
+                    SqlCommand cm = new SqlCommand("USP_LIST_MOVIMIENTO", cn);
+                    cm.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader dr = cm.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        Movimiento movi = new Movimiento();
+                        movi.id_movimiento = dr["id_movimiento"].ToInt();
+                        movi.id_propietario =Convert.ToInt32(dr["id_propietario"].ToString());
+                        movi.id_tipo=Convert.ToInt32( dr["id_tipo"].ToString());
+                        movi.fecha_registro = Convert.ToDateTime( dr["fecha_registro"].ToDateTime());
+                        movimiento_list.Add(movi);
+                    }
+                    cn.Close();
+                }
+                oHeader.estado = true;
+
+            }
+            catch (Exception ex)
+            {
+                oHeader.estado = false;
+                oHeader.mensaje = ex.Message;
+            }
+            movimiento_res.MovimientoList = movimiento_list;
+            movimiento_res.oHeader = oHeader;
+
+            return movimiento_res;
+        }
+
+    }
+}
---
+++ Negocio/IncidenteFile_BS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using Entidades;
+
+namespace Negocio
+{
+   public class IncidenteFile_BS
+    {
+        public IncidenteFile_Res lista()
+        {
+            return new IncidenteFile_DA().Listar();
+        }
+    }
+}
