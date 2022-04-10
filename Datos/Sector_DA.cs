c1173292b706e60f733d0d4f5b0dd2ee0339aec7
JimmyBarzola <jbarzolag7@gmail.com>
2022-04-10 18:11:38 -0500

    CRUD - SERVICIO Y SECTOR

--- Datos/Sector_DA.cs
+++ Datos/Sector_DA.cs
@@ -9,6 +9,7 @@ using System.Data.SqlClient;
 using Entidades;
 using Helpers;
 
+
 namespace Datos
 {
     public class Sector_DA
@@ -25,15 +26,15 @@ namespace Datos
                 using(SqlConnection cn= Conexion.Conectar())
                 {
                     cn.Open();
-                    SqlCommand cm = new SqlCommand("select * from SECTOR", cn);
+                    SqlCommand cm = new SqlCommand("USP_LIST_SECTOR", cn);
                     SqlDataReader dr = cm.ExecuteReader();
                     while (dr.Read())
                     {
                         Sector sector = new Sector();
-                        sector.id_sector = dr["id_sector"].ToInt();
+                        sector.id_sector = Convert.ToInt32(dr["id_sector"].ToString());
                         sector.nombre_sector = dr["nombre_sector"].ToString();
-                        sector.fecha_creacion = dr["fecha_creacion"].ToDateTime();
-                        sector.id_sucursal = dr["id_sucursal"].ToInt();
+                        sector.fecha_creacion = Convert.ToDateTime(dr["fecha_creacion"].ToString());
+                        sector.id_sucursal = Convert.ToInt32(dr["id_sucursal"].ToString());
                     }
                     cn.Close();
                 }
@@ -50,9 +51,61 @@ namespace Datos
             return sector_res;
         }
 
-        public void Listar2()
+        
+        public DTOHeader Registrar(Sector s)
         {
-
+            DTOHeader oHeader = new DTOHeader();
+            try
+            {
+                using (SqlConnection cn = Conexion.Conectar())
+                {
+                    cn.Open();
+                    SqlCommand cm = new SqlCommand("USP_INSERT_SECTOR", cn);
+                    cm.CommandType = CommandType.StoredProcedure;
+                    cm.Parameters.AddWithValue("@nombre_sector", s.nombre_sector);
+                    cm.Parameters.AddWithValue("@fecha_creacion", s.fecha_creacion);
+                    cm.Parameters.AddWithValue("@id_sucursal", s.id_sucursal);
+                    cm.ExecuteNonQuery();
+                    cn.Close();
+                }
+                oHeader.estado = true;
+            }
+            catch (Exception ex)
+            {
+                oHeader.estado = false;
+                oHeader.mensaje = ex.Message;
+            }
+            return oHeader;
         }
+
+        public DTOHeader Actualizar(Sector s)
+        {
+            DTOHeader oHeader = new DTOHeader();
+            try
+            {
+                using (SqlConnection cn = Conexion.Conectar())
+                {
+                    cn.Open();
+                    SqlCommand cm = new SqlCommand("USP_UPDATE_SECTOR", cn);
+                    cm.CommandType = CommandType.StoredProcedure;
+                    cm.Parameters.AddWithValue("@id_sector", s.id_sector);
+                    cm.Parameters.AddWithValue("@nombre_sector", s.nombre_sector);
+                    cm.Parameters.AddWithValue("@fecha_creacion", s.fecha_creacion);
+                    cm.Parameters.AddWithValue("@id_sucursal", s.id_sucursal);
+
+                    cm.ExecuteNonQuery();
+                    cn.Close();
+                }
+                oHeader.estado = true;
+            }
+            catch (Exception ex)
+            {
+                oHeader.estado = false;
+                oHeader.mensaje = ex.Message;
+            }
+            return oHeader;
+        }
+
     }
+
 }
