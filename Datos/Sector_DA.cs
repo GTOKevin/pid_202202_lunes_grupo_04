779e26c5b4855ff0d0ce32f3b288034d6b70df7b
JimmyBarzola <jbarzolag7@gmail.com>
2022-04-09 19:33:38 -0500

    Agregado de Datos, Entidades y Negocio - Solo Listar

---
+++ Datos/Sector_DA.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using Entidades;
+using Helpers;
+
+namespace Datos
+{
+    public class Sector_DA
+    {
+        public Sector_Res Listar()
+        {
+            Sector_Res sector_res = new Sector_Res();
+            List<Sector> sector_list = new List<Sector>();
+            DTOHeader oHeader = new DTOHeader();
+
+            try
+            {
+                using(SqlConnection cn= Conexion.Conectar())
+                {
+                    cn.Open();
+                    SqlCommand cm = new SqlCommand("select * from SECTOR", cn);
+                    SqlDataReader dr = cm.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        Sector sector = new Sector();
+                        sector.id_sector = dr["id_sector"].ToInt();
+                        sector.nombre_sector = dr["nombre_sector"].ToString();
+                        sector.fecha_creacion = dr["fecha_creacion"].ToDateTime();
+                        sector.id_sucursal = dr["id_sucursal"].ToInt();
+                    }
+                    cn.Close();
+                }
+                oHeader.estado = true;
+            }
+            catch (Exception ex)
+            {
+                oHeader.estado = false;
+                oHeader.mensaje = ex.Message;
+            }
+            sector_res.SectorList = sector_list;
+            sector_res.oHeader = oHeader;
+
+            return sector_res;
+        }
+    }
+}
---
+++ Datos/Servicio_DA.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using Entidades;
+using Helpers;
+
+namespace Datos
+{
+    public class Servicio_DA
+    {
+        public Servicio_Res Listar()
+        {
+            Servicio_Res servicio_res = new Servicio_Res();
+            List<Servicio> servicio_list = new List<Servicio>();
+            DTOHeader oHeader = new DTOHeader();
+
+            try
+            {
+                using(SqlConnection cn = Conexion.Conectar())
+                {
+                    cn.Open();
+                    SqlCommand cm = new SqlCommand("select * from SERVICIO", cn);
+                    SqlDataReader dr = cm.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        Servicio servicio = new Servicio();
+                        servicio.id_servicio = dr["id_servicio"].ToInt();
+                        servicio.id_tipo = dr["id_tipo"].ToInt();
+                        servicio.id_departamento = dr["id_departamento"].ToInt();
+                        servicio.nombre = dr["nombre"].ToString();
+                        servicio.fecha_registro = dr["fecha_registro"].ToDateTime();
+                        servicio_list.Add(servicio);
+                    }
+                    cn.Close();
+                }
+                oHeader.estado = true;
+            }
+
+            catch (Exception ex)
+            {
+
+                oHeader.estado = false;
+                oHeader.mensaje = ex.Message;
+            }
+            servicio_res.ServicioList = servicio_list;
+            servicio_res.oHeader = oHeader;
+
+            return servicio_res;
+        }
+    }
+}
