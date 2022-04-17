45c7fb7ce905a8d7a5e18cb94a5511ec47e64d7c
jimenasol <93685630+jimenasol@users.noreply.github.com>
2022-04-17 17:04:26 -0500

    Envio de mi crud de ROL y TIPO

--- Datos/Rol_DA.cs
+++ Datos/Rol_DA.cs
@@ -12,7 +12,7 @@ namespace Datos
 {
     public class Rol_DA
     {
-        public Rol_Res Listar()
+        public Rol_Res Listar(int id_rol)
         {
             Rol_Res rol_Res = new Rol_Res();
             List<Rol> rol_list = new List<Rol>();
@@ -22,25 +22,67 @@ namespace Datos
                 using (SqlConnection cn = Conexion.Conectar())
                 {
                     cn.Open();
-                    SqlCommand cm = new SqlCommand("USP_ROL_LISTAR", cn);
+                    SqlCommand cm = new SqlCommand("SP_ROL_LISTAR", cn);
                     cm.CommandType = CommandType.StoredProcedure;
+                    cm.Parameters.AddWithValue("@id_rol", id_rol);
                     SqlDataReader dr = cm.ExecuteReader();
                     while (dr.Read())
                     {
                         Rol rol = new Rol();
                         rol.id_rol=dr["id_rol"].ToInt();
                         rol.nombre = dr["nombre"].ToString();
+                        rol.descripcion = dr["descripcion"].ToString();
                         rol_list.Add(rol);     
                     }
                     cn.Close();
                 }
                 oHeader.estado = true;
-            }catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 oHeader.estado = false;
                 oHeader.mensaje = ex.Message;
             }
+            rol_Res.RolList = rol_list;
+            rol_Res.oHeader = oHeader;
             return rol_Res;
-        } 
-     }
+        }
+
+        public Rol_Register Registrar(Rol rol)
+        {
+            Rol_Register Rol = new Rol_Register();
+            DTOHeader oHeader = new DTOHeader();
+            int id_registrar = 0;
+            try
+            {
+                int rpta = 0;
+                using (SqlConnection cn = Conexion.Conectar())
+                {
+                    cn.Open();
+                    SqlCommand cmd = new SqlCommand("SP_CREAR_ROL", cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@id_rol", rol.id_rol);
+                    cmd.Parameters.AddWithValue("@nombre", rol.nombre);
+                    cmd.Parameters.AddWithValue("@descripcion", rol.descripcion);
+                    rpta = Convert.ToInt32(cmd.ExecuteScalar());
+                    cn.Close();
+
+                }
+                id_registrar = rpta;
+                oHeader.estado = true;
+                oHeader.mensaje = "Se registro el Rol :" + rol.nombre;
+
+
+            }
+            catch (Exception ex)
+            {
+                oHeader.estado = false;
+                oHeader.mensaje = ex.Message;
+            }
+
+           Rol.oHeader = oHeader;
+            Rol.id_registrar = id_registrar;
+            return Rol;
+        }
+    }
 }
---
+++ Web_Integrador/Scripts/Tipo/Mantenimiento.js
@@ -0,0 +1,182 @@
+﻿
+var colsName = ['ID', 'NOMBRE', 'UNIDAD'];
+var tipoList = [];
+
+const llenarVariable = (lista, option) => {
+    let newArray;
+    switch (option) {
+        case 'new':
+            for (i = 0; i < lista.length; i++) {
+                tipoList.push(lista[i]);
+            }
+            break;
+        case 'edit':
+            console.log("edit");
+            newArray = tipoList.map(tipo => tipo.id_tipo == lista[0].id_tipo ? lista[0] : tipo)
+            tipoList = newArray;
+            break;
+
+    }
+}
+
+const init = () => {
+    showLoading();
+    setColumns("example", colsName, true);
+
+    getListaTipo();
+    Swal.close();
+};
+
+const btnAction = (t, tipo) => {
+    switch (tipo) {
+        case 'new':
+            cleanForm();
+            $("#view-table").hide(500);
+            $("#view-form").show(1000);
+            break;
+        case 'cancel':
+            $("#view-form").hide(500);
+            $("#view-table").show(1000);
+            break;
+        case 'edit':
+            $("#view-table").hide(500);
+            $("#view-form").show(1000);
+
+            let id = ((t.parentElement).parentElement).parentElement.id;
+            getTipoId(id);
+
+            break;
+
+    }
+};
+
+const getListaTipo = () => {
+    $.ajax({
+        method: "GET",
+        url: urlGetTipo,
+        responseType: 'json',
+        success: async function (res) {
+            Swal.close();
+            if (res.oHeader.estado) {
+                await llenarVariable(res.TipoList, 'new');
+                await listTable(tipoList);
+            } else {
+                Swal.fire('Ooops!', res.oHeader.mensaje, 'error');
+            }
+        },
+        error: function (err) {
+            Swal.close();
+        }
+
+    });
+}
+const listTable = (res) => {
+    $('#example').DataTable({
+        destroy: true,
+        data: res,
+        columns: [{ data: "id_tipo" }, { data: "nombre" }, { data: "unidad" },
+        buttonsDatatTable("edit")],
+        rowId: "id_tipo",
+        columnDefs:
+            [
+                {
+                    "targets": 0,
+                    "visible": false,
+                }
+            ],
+        order: [[0, 'des']],
+        language: {
+            "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+        }
+    });
+};
+
+$("#view-form").on("submit", function (e) {
+    let id_tipo = document.getElementsByName("id_tipo")[0];
+
+    if (id_tipo.value == "" || id_tipo.value == undefined) {
+        id_tipo.value = 0;
+    }
+
+    e.preventDefault();
+    let formData = {};
+    let validate = true;
+    $("#view-form input").each(function (index) {
+        if (this.value.trim().length != 0) {
+            formData[this.name] = this.value;
+        } else {
+            validate = false;
+        }
+
+    });
+
+    if (validate) {
+        showLoading();
+
+        $.ajax({
+            method: "POST",
+            url: urlSaveTipo,
+            data: formData,
+            responseType: 'json',
+            success: async function (res) {
+                Swal.close();
+                let { TipoList, oHeader } = res;
+                if (oHeader.estado) {
+                    if (id_tipo.value == "0") {
+                        await llenarVariable(TipoList, 'new');
+                    } else {
+                        await llenarVariable(TipoList, 'edit');
+                    }
+
+                    await listTable(tipoList);
+                    Swal.fire('ok', oHeader.mensaje, 'success');
+                }
+
+            },
+            error: function (err) {
+                Swal.close();
+            }
+        });
+
+    }
+
+    $("#view-form").hide(500);
+    $("#view-table").show(1000);
+
+});
+
+const getTipoId = (id) => {
+    $.ajax({
+        method: "GET",
+        url: urlGetTipo + "?id_tipo=" + id,
+        responseType: 'json',
+        success: async function (res) {
+            let { TipoList, oHeader } = res;
+            if (oHeader.estado) {
+                await llenarCampos(TipoList);
+            } else {
+                Swal.fire('Ooops!', res.oHeader.mensaje, 'error');
+            }
+        },
+        error: function (err) {
+            Swal.close();
+        }
+    });
+}
+
+const llenarCampos = (list) => {
+
+    if (list.length > 0) {
+        $("#view-form input").each(function (ind) {
+            for (var propName in list[0]) {
+                if (this.name === propName) {
+                    this.value = list[0][propName];
+                }
+            }
+
+        });
+    }
+
+}
+
+init();
